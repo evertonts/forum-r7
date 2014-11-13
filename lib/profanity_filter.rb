6a4035c25a0f7a6d55c47e7144fd0b5d01215745
Everton <everton2x4@gmail.com>
2014-11-13 19:01:30 -0200

    Add words blacklist

--- lib/profanity_filter.rb
+++ lib/profanity_filter.rb
@@ -1,7 +1,12 @@
+# The profane words should be added to the blacklist.txt file
+
 class ProfanityFilter
+  include Singleton
+  
+  CONFIG_PATH = "#{Rails.root}/config"
+  
   def normalize(string)
     normalized_string = string.downcase
-    normalizer = YAML.load_file("#{Rails.root}/config/character_normilizer.yml")
     
     normalizer.each do |key, value|
       next if value.nil?
@@ -9,4 +14,22 @@ class ProfanityFilter
     end
     normalized_string
   end
+  
+  def sanitize!(string)
+    blacklist.each_line do |line|
+      to_sub = /\b#{line.strip}\b/
+      string.gsub!(to_sub, 'XXXX')
+    end
+    string
+  end
+  
+  private
+  
+  def blacklist
+    @blacklist ||= File.read("#{CONFIG_PATH}/blacklist.txt")
+  end
+  
+  def normalizer
+    @normalizer ||= YAML.load_file("#{CONFIG_PATH}/character_normalizer.yml")
+  end 
 end
--- spec/lib/profanity_filter_spec.rb
+++ spec/lib/profanity_filter_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
 describe ProfanityFilter do
+  
+  subject { ProfanityFilter.instance  } 
+  
   describe '#nomalize' do
+    before do 
+      allow(subject).to receive(:normalizer) { { 'a' => '@', 'b' => 'i3' } }
+    end
+    
     it 'changes @ to a' do
       expect(subject.normalize('lal@')).to eq 'lala'
     end
     
     it 'changes I3 to b' do
       expect(subject.normalize('I3olA')).to eq 'bola'
+    end
+  end
+  
+  describe '#sanitize!' do
+    before { allow(subject).to receive(:blacklist) { 'arma' } }
+    
+    it 'exchange arma for XXXX' do
+      expect(subject.sanitize!('arma')).to eq 'XXXX'
     end
   end
 end
